d21b93bfc4323595143f008d83a679d132973d03
Ramon p <rp499963@gmail.com>
2024-06-01 23:21:02 -0300

    Chess layer and printing the board

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,15 @@
 package application;
 
-import boardgame.Position;
+import boardgame.Board;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Position position = new Position(3,3);
-		System.out.println(position);
+		Board board = new Board(8, 8);
+		
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
 	}
-
 }
--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -2,14 +2,11 @@ package boardgame;
 
 public class Position {
 	private int row;
-	private int colum;
+	private int column;
 	
-	public Position() {
-	}
-	
-	public Position(int row, int colum) {
+	public Position(int row, int column) {
 		this.row = row;
-		this.colum = colum;
+		this.column = column;
 	}
 	
 	public int getRow() {
@@ -21,15 +18,15 @@ public class Position {
 	}
 
 	public int getColum() {
-		return colum;
+		return column;
 	}
 
-	public void setColum(int colum) {
-		this.colum = colum;
+	public void setColum(int column) {
+		this.column = column;
 	}
 	
 	@Override
 	public String toString() {
-		return row + ", " + colum;
+		return row + ", " + column;
 	}
 }
